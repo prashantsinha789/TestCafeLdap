c0f1bd02c21642c040111c7a8fcd502d1f3b0f77
prashantsinha789 <prashantsinha789@gmail.com>
2020-08-24 22:27:43 +0530

    Removed Creds
    All Client Data Removed

--- Mocks/testMock.js
+++ Mocks/testMock.js
@@ -3,22 +3,22 @@ import { Selector } from 'testcafe';
 export default class mockData {
   constructor() {
 
-    this.sf_username            = "sfadmindev";          //username for Stage & stageRC
-    this.username               = "ygold";                 //username for Starfish UI
-    this.password               = "hawa11";               //Password for Starfish Stage & stageRC
-    this.ldapPassword           = "8DJDgQPyBHZpmQRFv3Tg";               //Password for Starfish LDAP
+    this.sf_username            = "";          //username for Stage & stageRC
+    this.username               = "";                 //username for Starfish UI
+    this.password               = "";               //Password for Starfish Stage & stageRC
+    this.ldapPassword           = "";               //Password for Starfish LDAP
 
-    this.ldapUrl                = "ldaps://ldap-stagerc.starfishsolutions.com:10202";
-    this.ldapServiceUsername    = "cn=admin,dc=example,dc=org";
-    this.ldapbasedn             = "ou=Users,dc=example,dc=org";
-    this.ldapFilter             = "(&(objectClass=person)(uid=STARFISH_USERNAME))";
-    this.localLoginStrategy     = "com.starfish.officehours.controller.security.LdapLocalLoginStrategy";
-    this.DefaultLoginStrategy   = "com.starfish.officehours.controller.security.DefaultLocalLoginStrategy";
-    this.ldapServicePassword    = "FQwicCcC7}GL7mKUezBW";
-    this.tenantID               = "4";
+    this.ldapUrl                = "";
+    this.ldapServiceUsername    = "";
+    this.ldapbasedn             = "";
+    this.ldapFilter             = "";
+    this.localLoginStrategy     = "";
+    this.DefaultLoginStrategy   = "";
+    this.ldapServicePassword    = "";
+    this.tenantID               = "";
 
-    this.sfAdminUrl             = `https://admin-stage-rc.starfishsolutions.com/starfish-admin-stageRC/login.jsp`;
-    this.ldapTesterUrl          = `https://admin-stage-rc.starfishsolutions.com/starfish-admin-stageRC/tools/index.html`;
-    this.stageRCuRL             = `https://stage-rc.starfishsolutions.com/starfish-stageRC/support/login.html`;
+    this.sfAdminUrl             = ``;
+    this.ldapTesterUrl          = ``;
+    this.stageRCuRL             = ``;
   }
 }
